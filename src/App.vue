207d959558265af3cf26b9eba18af76e11af55b1
Anqi Li <425765498@qq.com>
2019-01-29 10:30:29 +0000

    Toolbar implemented

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,97 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="hide-overflow" style="position: relative;">
+      <v-toolbar absolute color="teal lighten-3" dark scroll-off-screen scroll-target="#scrolling-techniques">
+        <!--<v-toolbar-side-icon></v-toolbar-side-icon>-->
+        <v-btn icon>
+          <v-icon to="/">home</v-icon>
+        </v-btn>
+        <v-toolbar-title>Overseas Shopping</v-toolbar-title>
+
+        <v-spacer>
+          <v-autocomplete :loading="loading" :items="items" :search-input.sync="search"
+            v-model="search" cache-items class="mx-3" flat hide-no-data hide-details
+            label="Search" solo-inverted append-icon="search">
+          </v-autocomplete>
+        </v-spacer>
+
+        <v-btn icon>
+        <v-icon slot="badge" dark small>done</v-icon>
+        <v-icon large>account_circle</v-icon>
+        </v-btn>
+
+        <v-label>{{UserName}}</v-label>
+
+        <v-btn flat small to="/signin">Sign In</v-btn>
+        <v-btn flat small to="/signup">Sign Up</v-btn>
+
+        <v-btn icon>
+        <v-badge color="red">
+          <span slot="badge" small>{{itemsNum}}</span>
+          <v-icon >shopping_cart</v-icon>
+        </v-badge>
+        </v-btn>
+
+        <v-btn icon>
+        <v-badge color="orange">
+          <span slot="badge" small>{{notices}}</span>
+          <v-icon>mail</v-icon>
+        </v-badge>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <!--<div id="scrolling-techniques" class="scroll-y" style="max-height: 600px;">-->
+        <!--<v-container style="height: 1000px;"></v-container>-->
+      <!--</div>-->
+      <div id="body"></div>
+
+      <v-footer dark height="auto">
+        <v-card flat tile class="indigo lighten-1 white--text text-xs-center">
+          <v-card-text>
+            <v-btn v-for="icon in icons" :key="icon" class="mx-3 white--text" icon>
+              <v-icon size="24px">{{ icons }}</v-icon>
+            </v-btn>
+          </v-card-text>
+
+          <v-card-text class="white--text pt-0">
+            Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus
+            luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus.
+            Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel
+            elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-text class="white--text">&copy;2018 —
+            <strong>Vuetify</strong>
+          </v-card-text>
+        </v-card>
+      </v-footer>
+    </div>
+    <!--<router-view/>-->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      UserName: 'UserName',
+      itemsNum: 1,
+      notices: 10,
+      icons: [
+        'fab fa-facebook',
+        'fab fa-twitter',
+        'fab fa-google-plus',
+        'fab fa-linkedin',
+        'fab fa-instagram'
+      ]
+    }
+  }
 }
 </script>
 
@@ -18,6 +102,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import HelloWorld from '@/components/HelloWorld'
+import SignIn from '@/components/SignIn'
+import SignUp from '@/components/SignUp'
 
+Vue.use(Vuetify)
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +15,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/signin',
+      name: 'SignIn',
+      component: SignIn
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp
     }
   ]
 })
